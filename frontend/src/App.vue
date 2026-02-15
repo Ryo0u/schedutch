b547adfeea6c9c0cad34d5f6854419a83f4ffbb5
Ryo0u <ryouaren@gmail.com>
2026-02-15 11:30:00 +0900

    モーダルをスマホの場合に対応

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <header class="w-full bg-blue-50">
-    <div class="max-w-5xl mx-auto h-20 flex items-center justify-center sm:justify-start">
+    <div class="max-w-5xl mx-auto h-15 sm:h-20 flex items-center justify-center sm:justify-start">
       <img src="@/assets/AppLogo.png" class="h-30 md:h-35 w-auto object-contain"/>
     </div>
   </header>
--- frontend/src/components/show/ShowModal.vue
+++ frontend/src/components/show/ShowModal.vue
@@ -108,22 +108,22 @@ const submitData = async (data) => {
       class="bg-white rounded-xl shadow-2xl flex flex-col border-5 border-blue-100 overflow-hidden transition-all duration-300 pointer-events-auto"
       :class="isExpanded ? 'w-full max-w-4xl max-h-[90vh]' : 'w-60 shadow-lg'"
     >
-      <div class="bg-blue-100 flex items-center justify-between p-4 border-b border-gray-100 cursor-pointer" @click="isExpanded = true">
-        <h3 class="font-bold text-gray-500 truncate" :class="isExpanded ? 'text-xl' : 'text-xs'">
+      <div class="bg-blue-100 flex items-center justify-between p-2 sm:p-4 border-b border-gray-100 cursor-pointer" @click="isExpanded = true">
+        <h3 class="font-bold text-gray-500 truncate" :class="isExpanded ? 'text-lg sm:text-xl' : 'text-xs'">
           {{ isExpanded ? 'あなたの予定を入力する' : '入力を再開する' }}
         </h3>
         
-        <div class="flex item-center gap-2 shrink-0">
+        <div class="flex items-baseline gap-2 shrink-0">
           <button 
             @click.stop="toggleMinimize"
-            class="text-xl text-gray-400 hover:text-gray-600 w-8 h-8 flex items-center justify-center rounded-full hover:bg-gray-200 transition cursor-pointer"
+            class="text-md sm:text-xl text-gray-400 hover:text-gray-600 w-6 sm:w-8 h-6 sm:h-8 flex items-center justify-center rounded-full hover:bg-gray-200 transition cursor-pointer"
           >
             {{ isExpanded ? 'ー' : '◻︎' }}
           </button>
           
           <button
             @click="emit('closeModal')"
-            class="text-2xl text-gray-400 hover:text-gray-600 w-8 h-8 flex items-center justify-center rounded-full hover:bg-gray-200 transition pb-1.5  cursor-pointer"
+            class="text-lg sm:text-2xl text-gray-400 hover:text-gray-600 w-6 sm:w-8 h-6 sm:h-8 flex items-center justify-center rounded-full hover:bg-gray-200 transition pb-1.5  cursor-pointer"
           >
             ×
           </button>
@@ -131,27 +131,27 @@ const submitData = async (data) => {
         
       </div>
       
-      <div v-show="isExpanded" class="p-6 overflow-y-auto">
+      <div v-show="isExpanded" class="p-4 sm:p-6 overflow-y-auto">
         <div class="max-w-5xl mx-auto">	
-					<div class="border-b border-gray-200 pb-8 mb-3">
+					<div class="border-b border-gray-200 pb-5 sm:pb-8 mb-3">
 
 						<div class="flex flex-col md:flex-row gap-6 mb-6">
 							
 							<div class="w-full md:w-1/2 flex items-center gap-4">
 								<span class="w-20 text-sm font-bold text-gray-600 whitespace-nowrap shrink-0">名前:</span>
-								<input v-model="name"  type="text" class="w-full border-2 border-gray-400 rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-blue-500">
+								<input v-model="name"  type="text" class="w-full border-2 border-gray-400 rounded px-3 py-1 sm:py-2 text-gray-700 focus:outline-none focus:border-blue-500">
 							</div>
 
 							<div class="w-full md:w-1/2 flex items-center gap-4">
 								<span class="w-20 text-sm font-bold text-gray-600 whitespace-nowrap shrink-0">パスワード:</span>
-								<input v-model="password" type="text" class="w-full border-2 border-gray-400 rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-blue-500">
+								<input v-model="password" type="text" class="w-full border-2 border-gray-400 rounded px-3 py-1 sm:py-2 text-gray-700 focus:outline-none focus:border-blue-500">
 							</div>
 							
 						</div>
 
 						<div class="flex gap-4">
 							<span class="w-20 text-sm font-bold text-gray-600 whitespace-nowrap shrink-0 pt-2">コメント:</span>
-							<textarea v-model="comment" rows="2" class="w-full border-2 border-gray-400 rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-blue-500"></textarea>
+							<textarea v-model="comment" rows="2" class="w-full border-2 border-gray-400 rounded px-3 py-1 sm:py-2 text-gray-700 focus:outline-none focus:border-blue-500"></textarea>
 						</div>
 
 					</div>
@@ -167,7 +167,7 @@ const submitData = async (data) => {
           <div>
             <button
               @click="triggerSubmit"
-              class="w-full bg-blue-400 text-white text-md font-bold mt-4 py-3 px-6 rounded-full shadow-lg hover:bg-blue-500 hover:-translate-y-1 transition transform"
+              class="w-full bg-blue-400 text-white text-md font-bold  py-3 px-6 rounded-full shadow-lg hover:bg-blue-500 hover:-translate-y-1 transition transform"
             >
               予定を追加する
             </button>
