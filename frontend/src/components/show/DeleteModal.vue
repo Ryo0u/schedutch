ea4a34aab700dd0a3817838043dbf294d2d90276
Ryo0u <ryouaren@gmail.com>
2026-02-23 16:39:52 +0900

    予定登録・イベント削除中のローディング表示を追加

--- frontend/src/components/show/DeleteModal.vue
+++ frontend/src/components/show/DeleteModal.vue
@@ -1,7 +1,9 @@
 <script setup>
 import {ref} from "vue"
 
-const password =ref("")
+const password = ref("")
+const isLoading = ref(false)
+
 const emit = defineEmits(["closeDelete", "submitDelete"])
 </script>
 <template>
@@ -18,10 +20,26 @@ const emit = defineEmits(["closeDelete", "submitDelete"])
       />
 			
 			<button
-				@click="emit('submitDelete', password)"
-				class="w-full bg-white text-red-500 border border-red-200 font-bold py-1 rounded-lg hover:bg-red-50 hover:-translate-y-0.5 transition-all mb-3"
+				@click="emit('submitDelete', password), isLoading=true"
+				class="w-full border font-bold py-2 rounded-lg flex justify-center items-center transition-all mb-4"
+				:class="[
+					(!password || isLoading )
+						? 'bg-gray-50 text-red-300 border-red-100 cursor-not-allowed'
+						: 'bg-white text-red-500 border-red-200 hover:bg-red-50 hover:-translate-y-0.5'
+				]"
+				:disabled="!password || isLoading"
+				
 			>
-				削除する
+				<template v-if=isLoading>
+            <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="text-red-500 opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            削除中...
+				</template>
+				<template v-else>
+					削除する
+				</template>
 			</button>
 			
 			<button
--- frontend/src/components/show/ShowModal.vue
+++ frontend/src/components/show/ShowModal.vue
@@ -23,13 +23,14 @@ const props = defineProps({
 const emit = defineEmits(["closeModal"])
 
 const isExpanded = ref(true)
+const isEditMode = computed(() => !!props.editingUser)
+const isLoading = ref(false)
 
 const name = ref("")
 const password = ref("")
 const comment = ref("")
 const tableRef = ref(null)
 
-const isEditMode = computed(() => !!props.editingUser)
 
 onMounted(() => {
   if (isEditMode.value) {
@@ -45,6 +46,7 @@ const toggleMinimize = () => {
 
 const triggerSubmit = () => {
   tableRef.value?.submit()
+  isLoading.value = true
 }
 
 const submitData = async (data) => {
@@ -167,9 +169,24 @@ const submitData = async (data) => {
           <div>
             <button
               @click="triggerSubmit"
-              class="w-full bg-blue-400 text-white text-md font-bold  py-3 px-6 rounded-full shadow-lg hover:bg-blue-500 hover:-translate-y-1 transition transform"
+              class="w-full text-white text-md font-bold mt-3 py-3 px-6 rounded-full shadow-lg transition transform flex justify-center items-center"
+              :class="[
+                (isLoading || !name || !password)
+                  ? 'bg-blue-300 cursor-not-allowed'
+                  : 'bg-blue-400 hover:bg-blue-500 hover:-translate-y-1'
+              ]"
+              :disabled="isLoading || !name || !password"
             >
-              予定を追加する
+              <template v-if="isLoading">
+                <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                登録中...
+              </template>
+              <template v-else>
+                予定を追加する
+              </template>
             </button>
           </div>
 				</div>
