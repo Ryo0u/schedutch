177a70b694a37e4f90100f555294fc2389571cf8
Ryo0u <ryouaren@gmail.com>
2026-03-03 17:29:37 +0900

    テキストのコピー機能実装

--- frontend/src/components/show/ExtractTime.vue
+++ frontend/src/components/show/ExtractTime.vue
@@ -10,6 +10,8 @@ const includeTriangle = ref(false)
 const selectedUsers = ref([])
 const extractedTimes = ref("")
 
+const isCopied = ref(false)
+
 const extractTimes = () => {
 	if (selectedUsers.value.length === 0) {
 		extractedTimes.value = "メンバーを選択してください"
@@ -96,6 +98,16 @@ const formatDate = (dateStr) => {
 	return `${date.getMonth() + 1}/${date.getDate()}(${day})`
 }
 
+const copyText = async () => {
+	await navigator.clipboard.writeText(extractedTimes.value)
+	
+	isCopied.value = true
+	
+	setTimeout(() => {
+		isCopied.value = false
+	}, 1000)
+}
+
 </script>
 
 <template>
@@ -138,16 +150,41 @@ const formatDate = (dateStr) => {
       	</div>
 				
 				<div class="sm:w-2/3 flex flex-col">
-					<p class="text-xs font-medium text-gray-400 mb-2">
-						全員
-						<span v-if="includeTriangle">⚫︎または▲</span>
-						<span v-else>⚫︎</span>
-						の時間帯
-					</p>
+					<div class="flex justify-between items-center mb-2">
+						<p class="text-xs font-medium text-gray-400">
+							全員
+							<span v-if="includeTriangle">⚫︎または▲</span>
+							<span v-else>⚫︎</span>
+							の時間帯
+						</p>
+						
+						<button
+							@click="copyText"
+							class="shrink-0 text-sm text-gray-600"
+							:class="[
+								isCopied 
+								? 'cursor-default' 
+								: 'cursor-pointer hover:-translate-y-1 transform transition-all duration-200'
+							]"
+						>
+							<template v-if="isCopied">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-5">
+									<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+								</svg>
+							</template>
+								
+							<template v-else>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+								</svg>
+							</template>
+						</button>
+					</div>
 					<textarea 
 						readonly 
-						class="w-full grow  p-3 bg-gray-50 border rounded-lg text-sm font-mono focus:outline-none"
+						class="w-full grow  p-3 bg-gray-50 border rounded-lg text-sm font-mono focus:outline-none text-gray-700 placeholder-gray-400"
 						:value="extractedTimes"
+						:placeholder="`左のリストから予定を合わせたいメンバーを選び、「抽出する」ボタンを押してください。\n抽出した結果は、右上のコピーボタンからコピーしてLINEなどに貼り付けることができます。`"
 					></textarea>
       	</div>
 			</div>
