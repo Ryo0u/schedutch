cdaa06fc232dea1a5da85ca8ef9a563186262c52
Ryo0u <ryouaren@gmail.com>
2026-03-04 11:21:31 +0900

    ユーザーのモデル定義を変更

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   
   belongs_to :event
-  has_many :responses, dependent: :destroy
+  has_many :responses, dependent: :delete_all
   
   validates :name, presence: true 
 end
