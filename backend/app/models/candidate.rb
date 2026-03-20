b65ce9ff017325f3f000c79cc157a2edcc68b2c2
Ryo0u <ryouaren@gmail.com>
2026-03-20 18:59:47 +0900

    フォーマット

--- backend/app/models/candidate.rb
+++ backend/app/models/candidate.rb
@@ -1,7 +1,7 @@
 class Candidate < ApplicationRecord
   belongs_to :event
   has_many :responses, dependent: :delete_all
-  
+
   validates :start_time, presence: true
   validates :end_time, presence: true
 end
